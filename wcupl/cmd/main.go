f146f86889149ef728f366dd106eab8b29696f78
Wilfried Klaas <w.klaas@gmx.de>
2022-08-11 14:16:18 +0200

    WIP: restructuring main.go

--- wcupl/cmd/main.go
+++ wcupl/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -26,6 +27,7 @@ var (
 	simu     []string
 	mode     string
 	hasSimu  bool
+	cuplHome string
 )
 
 func init() {
@@ -38,14 +40,15 @@ func init() {
 	hasSimu = false
 }
 
-func main() {
-	log.Println("start")
-
-	cuplHome, ok := os.LookupEnv("CUPL_HOME")
+func parseParameters() {
+	ch, ok := os.LookupEnv("CUPL_HOME")
 	if !ok {
-		cuplHome = os.Args[0]
-		cuplHome = strings.TrimSuffix(cuplHome, "wcupl.exe")
+		ch = os.Args[0]
+		ch = strings.TrimSuffix(ch, "wcupl.exe")
 	}
+
+	cuplHome = ch
+
 	args := os.Args[1:]
 	for _, arg := range args {
 		log.Println("arg: ", arg)
@@ -55,109 +58,59 @@ func main() {
 			cuplargs = append(cuplargs, arg)
 		}
 	}
+}
+
+func main() {
+	log.Println("start")
+
+	parseParameters()
 
 	log.Println("cupl args:", cuplargs)
 	log.Println("file     :", file)
+
 	workdir := filepath.Join(filepath.Dir(file), "wcupl")
 	err := os.Mkdir(workdir, os.ModePerm)
 	if err != nil && !os.IsExist(err) {
 		log.Fatalf("error creating temp dir: %v\n", err)
 	}
+
 	destfile := filepath.Join(workdir, filepath.Base(file))
 
-	filename := strings.TrimSuffix(filepath.Base(file), filepath.Ext(file))
-
-	ctx := map[string]string{
-		"filename": filename,
-		"date":     time.Now().Format(layoutISO),
-	}
-
-	b, err := ioutil.ReadFile(file)
+	file, err = renderFile(file, destfile)
 	if err != nil {
-		log.Fatalf("error loading source file: %v\n", err)
+		log.Fatalf("error rendering file: %v\n", err)
 	}
 
-	result, err := raymond.Render(string(b), ctx)
+	err, headers, pld, simu := scanfile(file)
 	if err != nil {
-		log.Fatalf("error render file: %v\n", err)
+		log.Fatalf("error scanning file: %v\n", err)
 	}
 
-	err = ioutil.WriteFile(destfile, []byte(result), 0644)
+	hasSimu, err := validate(headers, pld, simu)
 	if err != nil {
-		log.Fatalf("error writing file: %v\n", err)
+		log.Fatalf("error validating file: %v\n", err)
 	}
 
-	file = destfile
-	f, err := os.Open(file)
-	if err != nil {
-		log.Fatalf("error opening file: %v\n", err)
-	}
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		line := s.Text()
-		if strings.HasPrefix(strings.ToLower(line), "header") {
-			mode = "hdr"
-			continue
-		}
-		if strings.HasPrefix(strings.ToLower(line), "pld") {
-			mode = "pld"
-			continue
-		}
-		if strings.HasPrefix(strings.ToLower(line), "simulator") {
-			mode = "sim"
-			continue
-		}
-		switch mode {
-		case "hdr":
-			headers = append(headers, line)
-		case "pld":
-			pld = append(pld, line)
-		case "sim":
-			simu = append(simu, line)
-		}
-	}
-
-	if len(headers) == 0 {
-		log.Fatal("no headers found")
-	}
-	if len(pld) == 0 {
-		log.Fatal("no pld found")
-	}
-	hasSimu = true
-	if len(simu) == 0 {
-		log.Print("no simulation found")
-		hasSimu = false
-	}
-	log.Println("starting file genration")
+	log.Println("starting file generation")
 	pldFile := strings.TrimSuffix(file, filepath.Ext(file)) + ".pld"
-
-	pld = append(headers, pld...)
-	err = WriteLines(pldFile, pld)
+	err = generateFile(pldFile, headers, pld)
 	if err != nil {
 		log.Fatalf("error creating file: %v\n", err)
 	}
 	if hasSimu {
 		siFile := strings.TrimSuffix(file, filepath.Ext(file)) + ".si"
-		simu = append(headers, simu...)
-		err = WriteLines(siFile, simu)
+		err = generateFile(siFile, headers, simu)
 		if err != nil {
 			log.Fatalf("error creating file: %v\n", err)
 		}
 	}
 
-	cuplargs = append(cuplargs, pldFile)
 	log.Println("starting cupl from ", cuplHome)
-	cupl := filepath.Join(cuplHome, "cupl.exe")
-	cmnd := exec.Command(cupl, cuplargs...)
-	var outb, errb bytes.Buffer
-	cmnd.Stdout = &outb
-	cmnd.Stderr = &errb
-	err = cmnd.Run()
-	log.Println(outb.String())
-	log.Println(errb.String())
+	err = startcupl(cuplargs, pldFile)
 	if err != nil {
 		log.Fatalf("error starting cupl: %v\n", err)
 	}
+
 	log.Println("finnished")
 }
 
@@ -175,17 +128,100 @@ func WriteLines(path string, lines []string) error {
 	return w.Flush()
 }
 
-func copy(src, dest string) (string, error) {
-	bytesRead, err := ioutil.ReadFile(src)
-
-	if err != nil {
-		return "", err
+func renderFile(file, destfile string) (string, error) {
+	filename := strings.TrimSuffix(filepath.Base(file), filepath.Ext(file))
+	ctx := map[string]string{
+		"filename": filename,
+		"date":     time.Now().Format(layoutISO),
 	}
 
-	err = ioutil.WriteFile(dest, bytesRead, 0644)
-
+	b, err := ioutil.ReadFile(file)
 	if err != nil {
-		return "", err
+		return destfile, fmt.Errorf("error loading source file: %v\n", err)
 	}
-	return dest, nil
+
+	result, err := raymond.Render(string(b), ctx)
+	if err != nil {
+		return destfile, fmt.Errorf("error render file: %v\n", err)
+	}
+
+	err = ioutil.WriteFile(destfile, []byte(result), 0644)
+	if err != nil {
+		return destfile, fmt.Errorf("error writing file: %v\n", err)
+	}
+	return destfile, nil
+}
+
+func scanfile(file string) (err error, headers, pld, simu []string) {
+
+	headers = make([]string, 0)
+	pld = make([]string, 0)
+	simu = make([]string, 0)
+
+	f, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v\n", err), nil, nil, nil
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := s.Text()
+		if strings.HasPrefix(strings.ToLower(line), "header:") {
+			mode = "hdr"
+			continue
+		}
+		if strings.HasPrefix(strings.ToLower(line), "pld:") {
+			mode = "pld"
+			continue
+		}
+		if strings.HasPrefix(strings.ToLower(line), "simulator:") {
+			mode = "sim"
+			continue
+		}
+		switch mode {
+		case "hdr":
+			headers = append(headers, line)
+		case "pld":
+			pld = append(pld, line)
+		case "sim":
+			simu = append(simu, line)
+		}
+	}
+
+	return
+}
+
+func validate(headers, pld, simu []string) (hasSimu bool, err error) {
+	if len(headers) == 0 {
+		err = errors.New("no headers found")
+	}
+	if len(pld) == 0 {
+		err = errors.New("no pld found")
+	}
+	hasSimu = true
+	if len(simu) == 0 {
+		log.Print("no simulation found")
+		hasSimu = false
+	}
+	return
+}
+
+func generateFile(file string, headers, part []string) error {
+	content := append(headers, part...)
+	return WriteLines(file, content)
+}
+
+func startcupl(cuplargs []string, pldFile string) error {
+	cuplargs = append(cuplargs, pldFile)
+	cupl := filepath.Join(cuplHome, "cupl.exe")
+	cmnd := exec.Command(cupl, cuplargs...)
+	var outb, errb bytes.Buffer
+	cmnd.Stdout = &outb
+	cmnd.Stderr = &errb
+	err := cmnd.Run()
+	log.Println(outb.String())
+	log.Println(errb.String())
+
+	return err
 }
--- wcupl/readme.md
+++ wcupl/readme.md
@@ -4,12 +4,64 @@ Since WinCUPL is no longer running on any of my computers, I wrote a batch, dire
 
 The tool itself doesn't do much. Any arguments are passed directly to CUPL. 
 
-- macros with {{}} will be exchange with some other data:
+- macros surrounded by `{{}}` will be exchange with some other data:
   `{{filename}}`: is the filename without wpld extension
   `{{date}}`: is the actual date in format yyyy-mm-dd (ISO8601)
 - it will create a subfolder called wcupl and copy the wpld into it. 
-- From this wpld files, `header:` and `pld:` are merged into one #.pld file and `header`: and `simulator:` part into the #.si file. 
+- From this wpld files, `header:` and `pld:` are merged into one #.pld file and `header`: and `simulator:` part into the #.si file.  The section names `header, pld, simulator` are case insensitive, so header and HEADER means the same.
 - Then cupl is started from the same directory where wcupl is located. Or if present using environment variable `CUPL_HOME` for trying to find cupl.exe. (So sorry, this version is only for windows users)
 
 
+
+Example wpld file:
+
+```wpld
+HEADER:
+Name     {{filename}} ;
+PartNo   01 ;
+Date     {{date}} ;
+Revision 02 ;
+Designer wkla ;
+Company  nn ;
+Assembly None ;
+Location  ;
+Device   G16V8 ;
+
+PLD:
+/* *************** INPUT PINS *********************/
+PIN 1   =  A12; 
+PIN 2   =  A13;
+PIN 3   =  A14;
+PIN 4   =  A15;
+PIN 9   =  PHI2;
+
+/* *************** OUTPUT PINS *********************/
+PIN 12   =  CSRAM;
+PIN 13   =  CSHIROM;
+PIN 15   =  CSIO;
+
+/* *************** LOGIC *********************/
+
+/* RAM */
+CSRAM = A15 # !PHI2;
+
+/* 8kb of ROM */
+CSHIROM = !(A15 & A14 & A13);
+
+/* IO */
+CSIO= !(A15 & A14 & !A13 & A12);
+
+SIMULATOR:
+/* Starting Test description */
+ORDER: A15, A14, A13, A12, PHI2, CSRAM, CSHIROM, CSIO; 
+
+VECTORS:
+0 X X X 0 H H H 
+0 X X X 1 L H H 
+1 0 X X X H H H 
+1 1 0 0 X H H H 
+1 1 0 1 X H H L 
+1 1 1 X X H L H 
+
+```
 
