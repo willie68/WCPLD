68c11f3fe7604c58339e28bc76faaf90eeaaa18b
Willie <w.klaas@gmx.de>
2022-08-13 11:28:56 +0200

    WIP: building CA version of dual hex with 16V8

--- WCPLD Examples/7-seg decoder/compile.cmd
+++ WCPLD Examples/7-seg decoder/compile.cmd
@@ -1,6 +1,6 @@
 @echo off
 set LIBCUPL=e:\Wincupl\Shared\atmel.dl
-e:\Wincupl\Shared\wcupl.exe -jaxfsl -m4 %1
+e:\Wincupl\Shared\wcupl.exe -jaxfsl -m2 %1
 if errorlevel 1 (
 echo errors found
 .\wcupl\%1.lst
